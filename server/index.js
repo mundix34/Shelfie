783fd287e90ddb5776c606de39d4870d54f34878
Rachel Kariuki <rachelk@Rachels-MacBook-Pro.local>
2018-07-13 13:08:25 -0600

    new

--- server/index.js
+++ server/index.js
@@ -3,12 +3,19 @@ const bodyParser = require('body-parser');
 const ct = require('./controller');
 const app = express();
 require('dotenv').config();
+const massive = require('massive');
+
 
 app.use(bodyParser.json());
 
-const port = 3005;
+const port = 3008;
+
+massive(process.env.CONNECTIONS).then(dbSet => {
+    app.set('db', dbSet)
+}).catch(err => console.log(err))
+
 app.listen(port, () => {
-    console.log(`listening on port ${port}`);
+    console.log(`listening on port: ${port}`);
     
 })
 
